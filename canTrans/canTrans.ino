c89b37120d59d4d8e76cecf02b469067a5eb3857
oxott <bryangsa@live.com>
2020-03-03 21:23:19 -0300

    Update canTrans.ino

--- canTrans/canTrans.ino
+++ canTrans/canTrans.ino
@@ -11,7 +11,8 @@ MCP2515 mcp2515(10);
 
 ThreadController cpu;
 
-Thread threadSendCan;
+Thread threadSendCan1;
+Thread threadSendCan2;
 
 void setup(){
   while (!Serial);
@@ -23,8 +24,13 @@ void setup(){
   mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ); //Sets CAN at speed 500KBPS and Clock 8MHz
   mcp2515.setNormalMode();
 
-  threadSendCan.setInterval(500);
-  threadSendCan.onRun(send);
+  threadSendCan1.setInterval(500);
+  threadSendCan1.onRun(send1);
+  threadSendCan2.setInterval(500);
+  threadSendCan2.onRun(send2);
+
+  cpu.add(&threadSendCan1);
+  cpu.add(&threadSendCan2);
 }
 
 void loop() 
@@ -32,8 +38,7 @@ void loop()
   cpu.run();  
 }
 
-
-void send(){
+void send1(){
   int pot1 = analogRead(0);
   canMsg.can_id  = 0x036;           //CAN id as 0x036
   canMsg.can_dlc = 8;               //CAN data length as 8
@@ -46,5 +51,20 @@ void send(){
   canMsg.data[6] = 0x00;
   canMsg.data[7] = 0x00;
   mcp2515.sendMessage(&canMsg);     //Sends the CAN message  
-  Serial.println(String(pot1) + " = " + String(canMsg.data[0], DEC) +  ", " + String(canMsg.data[1], DEC));
+  Serial.println("Send 1: " + String(pot1) + " = " + String(canMsg.data[0], DEC) +  ", " + String(canMsg.data[1], DEC));
+}
+void send2(){
+  int pot1 = analogRead(5);
+  canMsg.can_id  = 0x037;           //CAN id as 0x036
+  canMsg.can_dlc = 8;               //CAN data length as 8
+  canMsg.data[0] = highByte(pot1);               //Update humidity value in [0]
+  canMsg.data[1] = lowByte(pot1);               //Update temperature value in [1]
+  canMsg.data[2] = 0x22;            //Rest all with 0
+  canMsg.data[3] = 0x00;
+  canMsg.data[4] = 0x00;
+  canMsg.data[5] = 0x00;
+  canMsg.data[6] = 0x00;
+  canMsg.data[7] = 0x00;
+  mcp2515.sendMessage(&canMsg);     //Sends the CAN message  
+  Serial.println("Send 2: " + String(pot1) + " = " + String(canMsg.data[0], DEC) +  ", " + String(canMsg.data[1], DEC));
 }
